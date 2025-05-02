b6ce280d4b8bf8d00d581d7c1f60f6e2cdc0991b
Bruno <74840490+brunogoncalvesferreira@users.noreply.github.com>
2025-05-01 23:46:46 -0300

    Atualizar o page.tsx

--- src/pages/app/dashboard/page.tsx
+++ src/pages/app/dashboard/page.tsx
@@ -7,7 +7,7 @@ import { RecentSales } from './components/recent-sales'
 
 export function Dashboard() {
   return (
-    <div className='min-h-screen flex flex-col gap-10 p-10'>
+    <div className='min-h-screen flex flex-col gap-10 md:p-10 p-4'>
       <h1 className='text-4xl text-gray-100 font-semibold'>Dashboard</h1>
 
       <div className='grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-5'>
